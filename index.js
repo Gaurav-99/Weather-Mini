0b98aa61f2f10630ee3594b4e84502c8b27f3ad3
Gaurav Joshi <51323586+Gaurav-99@users.noreply.github.com>
2021-09-14 19:22:29 +0530

    Initial project

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const product = require("./api/product");
+
+
+//Setup express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use("/api/product", product);
+require("dotenv").config();
+
+//API key
+const apiKey = `${process.env.API_KEY}`;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+//default get request
+app.get("/", (req, res) => {
+  res.render("lander", { weatherData: 0, error: null });
+});
+
+
+app.post('/', (req, res) => {
+
+  let city = req.body.city;
+
+
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  request(url, (err, response, body) => {
+
+    if (err) {
+      res.render('lander', { weatherData: null, error: 'Error, please try again' });
+    } else {
+      let weatherData = JSON.parse(body);
+      console.log(weatherData);
+
+      if (weatherData.main == undefined) {
+        res.render('lander', { weatherData: null, error: 'Error, please try again' });
+      } else {
+
+        let place = `${weatherData.name}, ${weatherData.sys.country}`,
+          weatherTimezone = `${new Date(weatherData.dt * 1000)}`,
+          sunset = `${new Date(weatherData.sys["sunset"] * 1000)}`,
+          sunrise = `${new Date(weatherData.sys["sunrise"] * 1000)}`;
+
+        var timezone = `${weatherData["timezone"]}`;
+
+        if (weatherData.sys.country !== "IN") {
+          var ist = 19800;
+          weatherTimezone = `${new Date(weatherData.dt * 1000 - ist * 1000 + weatherData["timezone"] * 1000)}`;
+          sunset = `${new Date(weatherData.sys["sunset"] * 1000 - ist * 1000 + weatherData["timezone"] * 1000)}`;
+          sunrise = `${new Date(weatherData.sys["sunrise"] * 1000 - ist * 1000 + weatherData["timezone"] * 1000)}`;
+        }
+
+        console.log(" Timezone: ", weatherTimezone);
+        console.log(" Sunset: ", sunset);
+        console.log(" sunrise: ", sunrise);
+        /*         weatherTimezone = weatherTimezone.toUTCString();
+                sunset = sunset.toUTCString();
+                sunrise = sunrise.toUTCString(); */
+        let weatherTemp = `${weatherData.main.temp}`,
+          weatherPressure = `${weatherData.main.pressure}`,
+          weatherIcon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`,
+          weatherDescription = `${weatherData.weather[0].description}`,
+          humidity = `${weatherData.main.humidity}`,
+          clouds = `${weatherData.clouds.all}`,
+          visibility = `${weatherData.visibility}`,
+          main = `${weatherData.weather[0].main}`,
+          weatherFahrenheit;
+        weatherFahrenheit = (weatherTemp * 9) / 5 + 32;
+
+        function roundToTwo(num) {
+          return +(Math.round(num + "e+2") + "e-2");
+        }
+        weatherFahrenheit = roundToTwo(weatherFahrenheit);
+
+        res.render("weatherPage", {
+          weatherData,
+          place,
+          temp: weatherTemp,
+          sunset,
+          sunrise,
+          pressure: weatherPressure,
+          icon: weatherIcon,
+          description: weatherDescription,
+          timezone: weatherTimezone,
+          humidity,
+          fahrenheit: weatherFahrenheit,
+          clouds,
+          visibility,
+          main,
+          error: null,
+        });
+      }
+    }
+
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Weather app listening on port 3000!");
+});
